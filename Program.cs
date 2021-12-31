f76ac1a4f6bcfdae1f906db0ad53d7879e508d8c
barisdemirkap <barisdemirkap@gmail.com>
2021-12-31 14:49:10 +0300

    Parse

--- Program.cs
+++ Program.cs
@@ -37,10 +37,44 @@ static void Main(string[] args)
     float w = 10.3f;
     byte v = (byte)w ;
     Console.WriteLine("v:"+v) ;
+      // ToString Methodu
 
+    Console.WriteLine("*****ToString Methodu********") ;
+    int xx = 6;
+    string yy = xx.ToString();
+    Console.WriteLine("yy:"+yy) ;
+    string zz = 12.5f.ToString() ;
+      Console.WriteLine("zz:"+zz) ;
     
-}
+    // System.Convert
+    Console.WriteLine("*****System.Convert*******") ;
 
+    string s1 = "10" , s2 = "20" ;
+    int number1, number2 ;
+    int sum;
+
+    number1 = Convert.ToInt32(s1) ;
+    number2 = Convert.ToInt32(s2) ;
+    sum= number1+number2 ;
+    Console.WriteLine("sum:"+sum);
+
+    ParseMethod();
+
+}
+    public static void ParseMethod()
+    {
+        string text1 = "10" ;
+        string text2 = "10.25" ;
+        int int1 ;
+        double double1 ;
+
+        int1 = Int32.Parse(text1) ;
+        double1= Double.Parse(text2);
+
+        Console.WriteLine("int1:" + int1) ;    
+        Console.WriteLine("double1:" + double1) ;
+        
+    }
 }
 
 }
