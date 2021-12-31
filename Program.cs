d8ca82b5e6332b04f1085a736bbf123718244553
barisdemirkap <barisdemirkap@gmail.com>
2021-12-31 14:12:32 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace tip_donusumleri
+{
+class Program
+{
+static void Main(string[] args)
+{       // Implicit Conservesion (Bilincsiz Donusum)
+    Console.WriteLine("*****Implicit Conservation********") ;
+    byte a = 5;
+    sbyte b = 30; 
+    short c= 10;
+
+    int d= a + b+c ;
+
+    Console.WriteLine("d:" + d);
+
+    long h =d ;
+    Console.WriteLine("h:" + h);
+
+    float i= h ;
+    Console.WriteLine("i:" + i);
+
+    string e = "baris demirkap";
+    char f= 'k' ;
+    object g =e+f+d ;
+    Console.WriteLine("g:" + g);
+    // Explicit Conservesion (Bilincli Donusum)
+
+    Console.WriteLine("*****Explicit Conservation********") ;
+
+
+
+    
+}
+
+}
+
+}
